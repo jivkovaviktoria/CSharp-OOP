fa3734104b7feb6f0a568428011f05f0a4772b5b
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-10-24 11:16:40 +0300

    Solved the tasks from Inheritance - lab

---
+++ Inheritance - lab/StackOfStrings/StackOfStrings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty() => base.Count == 0;
+        public Stack<string> AddRange() => new Stack<string>();
+    }
+}
