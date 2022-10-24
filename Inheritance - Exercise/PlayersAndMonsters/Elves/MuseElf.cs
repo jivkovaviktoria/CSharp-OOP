886eb001987e9eb513908b29e89c6bcf54362038
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-10-24 11:52:46 +0300

    Solved "Players and monsters" from Inheritance - exercise

---
+++ Inheritance - Exercise/PlayersAndMonsters/Elves/MuseElf.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayersAndMonsters.Elves
+{
+    public class MuseElf : Elf
+    {
+        public MuseElf(string username, int level) : base(username, level)
+        {
+
+        }
+    }
+}
---
+++ Inheritance - Exercise/PlayersAndMonsters/Wizards/SoulMaster.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayersAndMonsters.Wizards
+{
+    public class SoulMaster : Wizard
+    {
+        public SoulMaster(string username, int level) : base(username, level)
+        {
+
+        }
+    }
+}
