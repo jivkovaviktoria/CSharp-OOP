669c040234a3d1e435c4701ae747f1b116aa35a6
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-10-24 12:26:06 +0300

    Solved "Need for speed" from Inheritance - exercise

---
+++ Inheritance - Exercise/NeedForSpeed/Cars/FamilyCar.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeedForSpeed.Cars
+{
+    public class FamilyCar : Car
+    {
+        public FamilyCar(int horsePower, double fuel) : base(horsePower, fuel)
+        {
+
+        }
+    }
+}
---
+++ Inheritance - Exercise/NeedForSpeed/Cars/SportCar.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeedForSpeed.Cars
+{
+    public class SportCar : Car
+    {
+        public SportCar(int horsePower, double fuel) : base(horsePower, fuel)
+        {
+            this.FuelConsumption = 10;
+        }
+    }
+}
