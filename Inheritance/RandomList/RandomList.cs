7d90c2b8f3d821c5c26f9aa546247e2a9e690892
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-10-24 11:09:52 +0300

    Solved "Random list" from Inheritance - lab

---
+++ Inheritance/RandomList/RandomList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        public string RandomString()
+        {
+            Random random = new Random();
+            int n = random.Next(0, Count);
+
+            string s = base[n];
+            base.RemoveAt(n);
+            return s;
+        }
+    }
+}
