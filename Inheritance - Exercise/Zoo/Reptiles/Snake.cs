2c0837943bd107f06ff8f4cbb81a89e797dc8afa
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-10-24 11:36:32 +0300

    Solved "Zoo" from Inheritance - exercise

---
+++ Inheritance - Exercise/Zoo/Reptiles/Snake.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo.Reptiles
+{
+    public class Snake : Reptile
+    {
+        public Snake(string name) : base(name)
+        {
+
+        }
+    }
+}
