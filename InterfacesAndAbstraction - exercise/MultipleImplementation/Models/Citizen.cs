164ebb75ebf5eec707ca97d3efe54cb5cc7e741e
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-11-01 11:12:56 +0200

    Solved "Multi implementation" from Interfaces and abstraction - exercise

---
+++ InterfacesAndAbstraction - exercise/MultipleImplementation/Models/Citizen.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonInfo
+{
+    public class Citizen : IPerson, IIdentifiable, IBirthable
+    {
+        public Citizen(string name, int age, string id, string birthDate)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Id = id;
+            this.Birthdate = birthDate;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Id { get; set; }
+        public string Birthdate { get; set; }
+    }
+}
---
+++ InterfacesAndAbstraction - exercise/MultipleImplementation/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PersonInfo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            string id = Console.ReadLine();
+            string birthDate = Console.ReadLine();
+
+            IIdentifiable identifiable = new Citizen(name, age, id, birthDate);
+            IBirthable birthable = new Citizen(name, age, id, birthDate);
+
+            Console.WriteLine(identifiable.Id);
+            Console.WriteLine(birthable.Birthdate);
+        }
+    }
+}
