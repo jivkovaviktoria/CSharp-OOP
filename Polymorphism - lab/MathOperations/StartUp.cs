92d6c032c7204bd44930887106205121fdc68310
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-11-08 17:30:08 +0200

    Solved "Math operations" from Polymorphism - lab

---
+++ Polymorphism - lab/MathOperations/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Operations
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            //Create a class MathOperations, which should have 3 times method Add(). Method Add() has to be invoked with:
+            //Add(int, int): int
+            //Add(double, double, double): double
+            //Add(decimal, decimal, decimal): decimal
+        }
+    }
+}
