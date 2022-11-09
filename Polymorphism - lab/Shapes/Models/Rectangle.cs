ce922b1b290bab066d93af86490c64c1793f1b7f
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-11-09 10:34:15 +0200

    Solved "Shapes" from Polymorphism - lab

---
+++ Polymorphism - lab/Shapes/Models/Rectangle.cs
@@ -0,0 +1,20 @@
+﻿namespace Shapes.Models
+{
+    public class Rectangle : Shape
+    {
+        private double height;
+        private double width;
+
+        public Rectangle(double height, double width)
+        {
+            this.height = height;
+            this.width = width;
+        }
+
+        public override double CalculateArea() => height * width;
+
+        public override double CalculatePerimeter() => 2 * (width + height);
+        
+        public override string Draw() => "Drawing " + this.GetType().Name;
+    }
+}
---
+++ Polymorphism - lab/Shapes/Models/Shape.cs
@@ -0,0 +1,10 @@
+﻿namespace Shapes.Models
+{
+    public abstract class Shape
+    {
+        public abstract double CalculatePerimeter();
+        public abstract double CalculateArea();
+
+        public virtual string Draw() => string.Empty;
+    }
+}
