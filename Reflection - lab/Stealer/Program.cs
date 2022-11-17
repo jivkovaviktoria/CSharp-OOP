e32d27d8edd46174f786ad019b38b10ac6651e35
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-11-17 12:22:38 +0200

    Solved "Stealer" from Reflection - lab

---
+++ Reflection - lab/Stealer/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Stealer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Spy spy = new Spy();
+            string result = spy.StealFieldInfo("Stealer.Hacker", "username", "password");
+            Console.WriteLine(result);
+        }
+    }
+}
