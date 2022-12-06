3a6964d141ad4537ade4d1ab7c85fb35c4276edf
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-12-06 14:02:31 +0200

    Solved "Command pattern" from Reflection - exercise

---
+++ Reflection - exercise/CommandPattern/Core/Commands/ExitCommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using CommandPattern.Core.Contracts;
+
+namespace CommandPattern.Core.Commands
+{
+    public class ExitCommand : ICommand
+    {
+        public string Execute(string[] input)
+        {
+            Environment.Exit(0);
+            return null;
+        }
+    }
+}
---
+++ Reflection - exercise/CommandPattern/Core/Commands/HelloCommand.cs
@@ -0,0 +1,9 @@
+﻿using CommandPattern.Core.Contracts;
+
+namespace CommandPattern.Core.Commands
+{
+    public class HelloCommand : ICommand
+    {
+        public string Execute(string[] input) => $"Hello, {input[0]}";
+    }
+}
